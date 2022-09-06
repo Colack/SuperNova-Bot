5ab4ab461bbdb342c183520eedb7cf312e62087f
Jack Spencer <UniformKitten281@gmail.com>
2022-09-06 08:22:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
+// Declare and Import Packages
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const fetch = require('node-fetch');
 
+// Client IDS and Tokens
 const clientID = "1011782808676606042";
 const { clientId, guildId, token } = require('./config.json');
 
+// Create a new client
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+// Create a commands collection
 client.commands = new Collection();
 
+// Get new commands from the 'commands' directory.
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+// Get new events from the 'events' directory.
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
+// Search the 'commands' folder for all command files.
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
 	client.commands.set(command.data.name, command);
 }
 
+// Search the 'events' folder for all event files.
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
 	const event = require(filePath);
@@ -32,13 +40,13 @@ for (const file of eventFiles) {
 	}
 }
 
+// Executed on Interactions
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
 	const command = interaction.client.commands.get(interaction.commandName);
 
 	if (!command) return;
-
 	try {
 		await command.execute(interaction);
 	} catch (error) {
@@ -47,4 +55,5 @@ client.on('interactionCreate', async interaction => {
 	}
 });
 
+// Login into Discord
 client.login(token);
