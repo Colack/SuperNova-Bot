d41da480d8d4054b043406d68a134e35ddf8b192
Jack Spencer <UniformKitten281@gmail.com>
2022-09-06 06:25:01 -0700

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,34 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { SlashCommandBuilder, Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+
+const { clientId, guildId } = require('./config.json');
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env['key']);
+
+rest.put(Routes.applicationCommands(clientId), { body: commands })
+	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+	.catch(console.error);
+
+/*
+  // Deleting Specific Guild's Commands
+	rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+		.then(() => console.log('Successfully deleted all guild commands.'))
+		.catch(console.error);
+
+  // Deleting all Global Commands
+	rest.put(Routes.applicationCommands(clientId), { body: [] })
+		.then(() => console.log('Successfully deleted all application commands.'))
+		.catch(console.error);
+*/
