5ed5374e774f528684e33b66a4e43645a25fa78e
Jack Spencer <UniformKitten281@gmail.com>
2022-11-12 14:52:35 -0800

    Create bot.js

---
+++ config/bot.js
@@ -0,0 +1,10 @@
+module.exports = {
+	emojis: {
+		think: ":5771hmmm:",
+		knife: ":5083pepeknife:",
+		error: ":9550idk:"
+	},
+	images: {
+		pfp: "https://github.com/Colack/SuperNova-Bot/blob/main/assets/Colack_A_supernova_exploding_with_code_fec0a35f-a74b-4317-b3cb-afcc34593082.png?raw=true"
+	}
+}
