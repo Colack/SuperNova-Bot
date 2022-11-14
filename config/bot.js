a199e3dc07da766ec6f266f19be013506ebd160b
Jack Spencer <UniformKitten281@gmail.com>
2022-11-13 19:48:40 -0800

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -1,8 +1,8 @@
 module.exports = {
 	emojis: {
-		think: ":5771hmmm:",
-		knife: ":5083pepeknife:",
-		error: ":9550idk:"
+		think: "",
+		knife: "",
+		error: ""
 	},
 	images: {
 		pfp: "https://github.com/Colack/SuperNova-Bot/blob/main/assets/Colack_A_supernova_exploding_with_code_fec0a35f-a74b-4317-b3cb-afcc34593082.png?raw=true"
