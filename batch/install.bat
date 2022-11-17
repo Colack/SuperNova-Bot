1c4123a8e05b6cb30ef26e24caf9164aef60a795
Jack Spencer <UniformKitten281@gmail.com>
2022-11-17 10:17:31 -0800

    Update install.bat

--- batch/install.bat
+++ batch/install.bat
@@ -1,7 +1,16 @@
+@ECHO OFF
+: Package Installer
+
 cd ../
+powershell write-host -fore green Installing Packages...
 npm i @discordjs/rest
 npm i @napi-rs/canvas
 npm i @types/node
 npm i discord.js
 npm i express
 npm i node-fetch
+powershell write-host -fore green Job completed!
+
+echo Press any key to Exit...
+time 9999 > NUL
+EXIT /B 0
